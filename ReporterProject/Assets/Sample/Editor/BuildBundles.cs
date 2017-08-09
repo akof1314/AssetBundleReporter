0b5910b9994a03fb8291a284a4cdcd055fc117ff
WuHuan <kof1234@sina.com>
2017-08-09 11:43:56 +0800

    demo脚本

--- ReporterProject/Assets/Sample/Editor/BuildBundles.cs
+++ ReporterProject/Assets/Sample/Editor/BuildBundles.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using UnityEditor;
+using WuHuan;
 
 public class BuildBundles
 {
@@ -21,7 +23,13 @@ public class BuildBundles
         {
             Directory.CreateDirectory(path);
         }
+#if UNITY_5 || UNITY_5_3_OR_NEWER
         BuildPipeline.BuildAssetBundles(path, BuildAssetBundleOptions.UncompressedAssetBundle, GetBuildTarget());
+#else
+        path += "/all.assetbundle";
+        UnityEngine.Object[] selectedAsset = Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.DeepAssets);
+        BuildPipeline.BuildAssetBundle(null, selectedAsset, path, BuildAssetBundleOptions.CollectDependencies, GetBuildTarget());
+#endif
     }
 
     public static BuildTarget GetBuildTarget()
@@ -38,5 +46,28 @@ public class BuildBundles
         string bundlePath = Path.Combine(directoryPath, GetBuildTarget().ToString());
         string outputPath = Path.Combine(directoryPath, GetBuildTarget().ToString() + DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".xlsx");
         WuHuan.AssetBundleReporter.AnalyzePrint(bundlePath, outputPath, () => System.Diagnostics.Process.Start(outputPath));
+    }
+
+    public static void MyAnalyzeCustomDepend()
+    {
+        AssetBundleFilesAnalyze.analyzeExport = true;
+        AssetBundleFilesAnalyze.analyzeCustomDepend = directoryPath =>
+        {
+            List<AssetBundleFileInfo> infos = new List<AssetBundleFileInfo>();
+
+            // 添加每个 AssetBundle 信息
+            AssetBundleFileInfo info = new AssetBundleFileInfo
+            {
+                //name = bundle,
+                //path = path,
+                //rootPath = directoryPath,
+                //size = new FileInfo(path).Length,
+                //directDepends = assetBundleManifest.GetDirectDependencies(bundle),
+                //allDepends = assetBundleManifest.GetAllDependencies(bundle)
+            };
+            infos.Add(info);
+
+            return infos;
+        };
     }
 }
